5fa6b2cde9423412d35363ca5bdb686c0136dfac
Ricardo Gonzalez <ricardo.gonzalezjr@colorado.edu>
2025-03-25 06:12:10 +0000

    implemented methods and handler to process user registrations on the POST register endpoint

--- src/main/java/Controller/SocialMediaController.java
+++ src/main/java/Controller/SocialMediaController.java
@@ -1,5 +1,11 @@
 package Controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import Model.Account;
+import Service.AccountService;
+import Service.MessageService;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 
@@ -9,6 +15,15 @@ import io.javalin.http.Context;
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
 public class SocialMediaController {
+    AccountService accService;
+    MessageService msgService;
+
+    public SocialMediaController() {
+        this.accService = new AccountService();
+        this.msgService = new MessageService();
+    }
+
+
     /**
      * In order for the test cases to work, you will need to write the endpoints in the startAPI() method, as the test
      * suite must receive a Javalin object from this method.
@@ -18,36 +33,29 @@ public class SocialMediaController {
         Javalin app = Javalin.create();
         app.get("example-endpoint", this::exampleHandler);
 
-        // TODO: POST endpoint "register"
-        app.post("/register", null);
+        // done: POST endpoint "register"
+        app.post("/register", this::registrationHandler);
 
-        // the body will contain a representation of a JSON Account, but will not contain an account_id.
-        // the registration will be successful if and only if the username is NOT blank, the password is AT LEAST 4 characters long, and an Account with that username does not already exist
-        // If all these conditions are met, the RESPONSE BODY should contain a JSON of the Account, including its account_id
-        // The response status should be 200 OK, which is the default
-        // The new account should be persisted to the database
-        // If the registration is not successful, the response status should be 400 (client error)
+        // // TODO: POST endpoint "login"
+        // app.post("/login", null);
 
-        // TODO: POST endpoint "login"
-        app.post("/login", null);
+        // // TODO: POST endpoint "messages"
+        // app.post("/messages", null);
 
-        // TODO: POST endpoint "messages"
-        app.post("/messages", null);
+        // // TODO: GET endpoint "messages"
+        // app.get("/messages", ctx -> ctx.json("GET messages endpoint"));
 
-        // TODO: GET endpoint "messages"
-        app.get("/messages", ctx -> ctx.json("GET messages endpoint"));
+        // // TODO: GET endpoint "messages/{message_id}"
+        // app.get("/messages/{message_id}", ctx -> ctx.json("GET messages by message ID endpoint"));
 
-        // TODO: GET endpoint "messages/{message_id}"
-        app.get("/messages/{message_id}", ctx -> ctx.json("GET messages by message ID endpoint"));
+        // // TODO: DELETE endpoint "messages/{message_id}"
+        // app.delete("/messages/{message_id}", null);
 
-        // TODO: DELETE endpoint "messages/{message_id}"
-        app.delete("/messages/{message_id}", null);
+        // // TODO: PATCH endpoint "messages/{message_id}"
+        // app.patch("/messages/{message_id}", null);
 
-        // TODO: PATCH endpoint "messages/{message_id}"
-        app.patch("/messages/{message_id}", null);
-
-        // TODO: GET endpoint "accounts/{account_id}/messages"
-        app.get("/accounts/{account_id}/messages", ctx -> ctx.json("GET messages for account by account ID endpoint"));
+        // // TODO: GET endpoint "accounts/{account_id}/messages"
+        // app.get("/accounts/{account_id}/messages", ctx -> ctx.json("GET messages for account by account ID endpoint"));
 
         return app;
     }
@@ -58,6 +66,38 @@ public class SocialMediaController {
      */
     private void exampleHandler(Context context) {
         context.json("sample text");
+    }
+
+    /**
+     * Handler to register a new user account.
+     * When successfully registered within the database, returns a JSON represation of the newly inserted Account with status code 200
+     * If something went wrong with registration, returns status code 400
+     * 
+     * ObjectMapper is used to convert the JSON from the request body into an Account object
+     * @param ctx - automatically provided by Javalin in order to handle HTTP requests and create HTTP responses
+     * @throws JsonProcessingException - thrown if there is an issue converting JSON into an object
+     */
+    private void registrationHandler(Context ctx) throws JsonProcessingException {
+        // iniializes object mapper convert request body's JSON into an object
+        ObjectMapper om = new ObjectMapper();
+        String accountJSON = ctx.body();
+        Account newUser = om.readValue(accountJSON, Account.class);
+
+        // utilizes account service to register a new user account
+        newUser = accService.registerUserAccount(newUser);
+
+        // if the registration wasn't successful, newUser will become null
+        if (newUser == null) {
+            // sets the response's status code to 400 to signal an error
+            ctx.status(400);
+        }
+        // else the registration was successful
+        else {
+            // sets the status code to 200, indicating success
+            ctx.status(200);
+            // returns the newly registered account information as a JSON
+            ctx.json(newUser);
+        }
     }
 
 
--- src/main/java/Service/AccountService.java
+++ src/main/java/Service/AccountService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import DAO.AccountDAO;
+import Model.Account;
 
 public class AccountService {
     private AccountDAO accountDAO;
@@ -10,4 +11,25 @@ public class AccountService {
         this.accountDAO = new AccountDAO();
     }
     
+    /**
+     * Registers a new user account within the Account table if all requirements were met
+     * 
+     * 1. username must not be blank
+     * 2. password must be at least 4 characters long
+     * 3. username must not already be in use within the Account table 
+     * 
+     * @param userAcc - an Account object without an account_id to be registered within the Account table
+     * @return A fully populated Account object (has its account_id) on successful insertion, or null on requirements failure / insertion failure
+     */
+    public Account registerUserAccount(Account userAcc) {
+        // the username must not be blank, password must be at least 4 characters long, and an Account with a matching username must not already exist
+        boolean newUserRequirements = !(userAcc.getUsername().isBlank()) && (userAcc.getPassword().length() >= 4) && !(this.accountDAO.usernameAlreadyExists(userAcc.getUsername()));
+
+        // if all the new user account requirements were met, then attempt to register them
+        if (newUserRequirements) {
+            return this.accountDAO.insertUser(userAcc);
+        }
+
+        return null;
+    }
 }
